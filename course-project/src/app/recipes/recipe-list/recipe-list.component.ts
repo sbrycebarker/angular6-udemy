5e7a107455793dfbe8ee085d80d6e30e6cb484e7
sbrycebarker <sbrycesti@gmail.com>
2018-10-17 00:28:17 -0600

    deets

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'Test Data',
+      'https://toriavey.com/images/2015/04/Brisket-6-1-600x800.jpg'),
+    new Recipe(
+      'Test Recipe',
+      'Test Data',
+      'https://toriavey.com/images/2015/04/Brisket-6-1-600x800.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
